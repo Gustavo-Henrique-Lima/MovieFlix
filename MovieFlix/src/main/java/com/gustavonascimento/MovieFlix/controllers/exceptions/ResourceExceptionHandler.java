3ccf5c86a5434076f32b4085b2beef86c552dbea
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-28 16:35:44 -0300

    feature: Creating Custom Exceptions.

---
+++ MovieFlix/src/main/java/com/gustavonascimento/MovieFlix/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gustavonascimento.MovieFlix.controllers.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.gustavonascimento.MovieFlix.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Recurso não encontrado");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+	}
+}
